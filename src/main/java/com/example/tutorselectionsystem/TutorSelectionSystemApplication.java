fb301b241b49f71f71cbe90ea5cb436d2dadbabd
nancy <957382789@qq.com>
2020-03-15 23:27:59 +0800

    Initialization configuration

---
+++ src/main/java/com/example/tutorselectionsystem/TutorSelectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorselectionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorSelectionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorSelectionSystemApplication.class, args);
+    }
+
+}
