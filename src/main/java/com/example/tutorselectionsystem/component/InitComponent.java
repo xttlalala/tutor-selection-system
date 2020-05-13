435093a800b0e2153509ff5d28e37f9e3e132be8
nancy <957382789@qq.com>
2020-05-13 21:30:09 +0800

    the project is completed.

--- src/main/java/com/example/tutorselectionsystem/component/InitComponent.java
+++ src/main/java/com/example/tutorselectionsystem/component/InitComponent.java
@@ -22,11 +22,27 @@ public class InitComponent implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         int number = 1001;
+        int number2 = 1002;
         User user = userService.getUser(number);
+        User user2 = userService.getUser(number2);
         if(user ==null){
             User u = new User();
             u.setName("BO");
             u.setNumber(number);
+            u.setRole(User.Role.TUTOR);
+            u.setPassword(encoder.encode(String.valueOf(number)));
+
+            Tutor t = new Tutor();//注意：tutor和user是组合关系，
+            // 直接持久化tutor是不行的 save User之后才能save Tutor
+
+//            userService.addTutor(u,t);//也可以使用级联的方法
+            t.setUser(u);
+            userService.addTutor(t);
+        }
+        if(user2 ==null){
+            User u = new User();
+            u.setName("Lily");
+            u.setNumber(number2);
             u.setRole(User.Role.TUTOR);
             u.setPassword(encoder.encode(String.valueOf(number)));
 
